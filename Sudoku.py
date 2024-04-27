99bfad8ebb99d250cf1ebc126147e7cbec4cb581
Aditya Behrani <adityabehrani@gmail.com>
2024-04-26 22:40:36 -0400

    encapsulated functions within class PuzzleSolver

--- Sudoku.py
+++ Sudoku.py
@@ -1,207 +1,185 @@
+"""Used to solve 9x9 Sudoku puzzles.
+
+All relavent functions to puzzle solving are in
+the PuzzleSolver class and hte main funcion will except
+singlular puzzles or a text file with multiple.
+Refer to the README for more information.
+"""
 import sys
 import time
-args = sys.argv[1:]
 
 
-# globals
-global N, LENGTH, WIDTH, SYMSET
-N = 9
-LENGTH = 3
-WIDTH = 3
-SYMSET = set('123456789')
+class PuzzleSolver:
+    """Class encapsulates all necessary functions to solve a 9x9 sudoku puzzle"""
+    def __init__(self):
+        self.n = 9
+        self.length = 3
+        self.width = 3
 
+    def create_constraints(self, puzzle: str) -> dict[int, set]:
+        """Map possible values for a given cell"""
+        constraints = {}
 
-def create_constraints(puzzle: str) -> dict[int, set]:
-    """ Creates a dictionary mapping the possible 
-        values for a given cell to each position
-    """
+        for cell in range(self.n**2):
+            if puzzle[cell] != ".":
+                continue
 
-    constraints = {}
+            constraints[cell] = (
+                self.row_constraints(puzzle, cell)
+                & self.col_constraints(puzzle, cell)
+                & self.box_constraints(puzzle, cell)
+            )
+        return constraints
 
-    for cell in range(N**2):
-        if puzzle[cell] != '.':
-            continue
+    def row_constraints(self, puzzle: str, cell: int) -> set:
+        """Map possible values for a given cell based on row"""
+        row_output = set("123456789")
+        row = cell // self.n
 
-        constraints[cell] = (row_constraints(puzzle, cell)
-                           & col_constraints(puzzle, cell)
-                           & box_constraints(puzzle, cell))
-    return constraints
+        for offset in range(self.n):
+            idx = row * self.n + offset
 
+            if puzzle[idx] != ".":
+                row_output.remove(puzzle[idx])
 
-def row_constraints(puzzle: str, cell: int) -> set:
-    """ Defines the possible values for a cell based on
-        values in its row
-    """
+        return row_output
 
-    row_output = set('123456789')
-    row = cell // N
+    def col_constraints(self, puzzle: str, cell: int) -> set:
+        """Map possible values for a given cell based on column"""
+        col_output = set("123456789")
+        col = cell % self.n
 
-    for offset in range(N):
-        idx = row * N + offset
+        for offset in range(self.n):
+            idx = col + self.n * offset
 
-        if puzzle[idx] != '.':
-            row_output.remove(puzzle[idx])
+            if puzzle[idx] != ".":
+                col_output.remove(puzzle[idx])
 
-    return row_output
+        return col_output
 
+    def box_constraints(self, puzzle: str, cell: int) -> set:
+        """Map possible values for a given cell based on box"""
+        box_output = set("123456789")
+        box_row = cell // (self.n * self.length)
+        box_col = cell % self.n // self.width
 
-def col_constraints(puzzle: str, cell: int) -> set:
-    """ Defines the possible values for a cell based on
-        values in its column
-    """
+        for x in range(self.length):
+            start = (box_row * self.length + x) * self.n + box_col * self.width
+            for i in range(start, start + self.width):
+                if puzzle[i] != ".":
+                    box_output.remove(puzzle[i])
 
-    col_output = set('123456789')
-    col = cell % N
+        return box_output
 
-    for offset in range(N):
-        idx = col + N * offset
+    def is_invalid(self, constraints: dict[int, set]) -> bool:
+        """Checks if current placement of numbers is valid"""
+        for cell in constraints:
+            if len(constraints[cell]) == 0:
+                return True
+        return False
 
-        if puzzle[idx] != '.':
-            col_output.remove(puzzle[idx])
+    def check_sum(self, puzzle: str) -> int:
+        """Sums the digits of a puzzle.
 
-    return col_output
-
-
-def box_constraints(puzzle: str, cell: int) -> set:
-    """ Defines the possible values for a cell based on
-        values in its box
-    """
-
-    box_output = set('123456789')
-    box_row = cell // (N * LENGTH)
-    box_col = cell % N // WIDTH
-
-    for x in range(LENGTH):
-        start = (box_row * LENGTH + x) * N + box_col * WIDTH
-        for i in range(start, start+WIDTH):
-            if puzzle[i] != '.':
-                box_output.remove(puzzle[i])
-
-    return box_output
-
-
-def is_invalid(constraints: dict[int, set]) -> bool:
-    """ Checks if the current set of constraints allows
-        for a puzzle to be completed
-    """
-
-    for cell in constraints:
-        if len(constraints[cell]) == 0:
-            return True
-    return False
-
-
-def check_sum(puzzle: str) -> int:
-    """ Sums the digits of a puzzle to make sure they sum to 405
         Used as a secondary check to make sure the solution is valid
-    """
+        and that the digits sum to a toal of 405
+        """
+        tot = 0
+        for char in puzzle:
+            if char != ".":
+                tot += int(char)
+        return tot
 
-    tot = 0
-    for char in puzzle:
-        if char != '.':
-            tot += int(char)
-    return tot
+    def is_solved(self, puzzle: str) -> bool:
+        """Checks for any remaining empty spaces in the puzzle"""
+        return "." not in puzzle
 
+    def best_choice(self, constraints: dict[int, set]) -> int:
+        """Finds the most constrained cell and returns its index"""
+        most_constrained_length = float("inf")
+        idx = -1
 
-def is_solved(puzzle: str) -> bool:
-    """ Checks for any remaining empty spaces in the puzzle
-    """
+        for cell in constraints:
+            if most_constrained_length > len(constraints[cell]):
+                most_constrained_length = len(constraints[cell])
+                idx = cell
 
-    return '.' not in puzzle
+        return idx
 
+    def choices(self, puzzle: str, constraints: dict[int, set]) -> list[str]:
+        """Creates potential puzzles based on current constraints"""
+        output = []
+        pos = self.best_choice(constraints)
 
-def best_choice(constraints: dict[int, set]) -> int:
-    """ Finds the most constrained cell and returns its index
-    """
+        for char in constraints[pos]:
+            output.append(str(puzzle[:pos] + char + puzzle[pos + 1 :]))
 
-    most_constrained_length = float('inf')
-    idx = -1
+        return output
 
-    for cell in constraints:
-        if most_constrained_length > len(constraints[cell]):
-            most_constrained_length = len(constraints[cell])
-            idx = cell
+    def solve(self, puzzle: str, constraints: dict[int, set]) -> str:
+        """Main function used to solve puzzles"""
+        if self.is_invalid(constraints):
+            return ""
 
-    return idx
+        if self.is_solved(puzzle):
+            return puzzle
 
+        for choice in self.choices(puzzle, constraints):
+            res = self.solve(choice, self.create_constraints(choice))
+            if res:
+                return res
 
-def choices(puzzle: str, constraints: dict[int, set]) -> list[str]:
-    """ Creates potential puzzles based on current constraints
-        and returns a list
-    """
-    output = []
-    pos = best_choice(constraints)
-
-    for char in constraints[pos]:
-        output.append(str(puzzle[:pos] + char + puzzle[pos+1:]))
-
-    return output
-
-
-def solve(puzzle: str, constraints: dict[int, set]) -> str:
-    """ Main function used to solve puzzles
-    """
-
-    if is_invalid(constraints):
         return ""
 
-    if is_solved(puzzle):
-        return puzzle
+    def run_file(self, filename: str) -> None:
+        """Tries to solve every puzzle in the provided txt file"""
+        tot_start = time.time()
 
-    for choice in choices(puzzle, constraints):
-        res = solve(choice, create_constraints(choice))
-        if res:
-            return res
+        puzzle_list = []
+        with open(filename, "r", encoding="ascii") as file:
+            puzzle_list = file.readlines()
 
-    return ""
+        for idx, puzzle in enumerate(puzzle_list):
+            start = time.time()
+            puzzle = puzzle.strip()
+            solution = self.solve(puzzle, self.create_constraints(puzzle))
 
+            print(
+                f"{f'Puzzle {idx}':<12}: {puzzle}\n"
+                + f"{'Solution':<12}: {solution}\n"
+                + f"{'Time':<12}: {str(time.time() - start)} seconds\n"
+            )
 
-def run_file(filename: str) -> None:
-    """ Tries to solve every puzzle in the provided txt file
-    """
-    tot_start = time.time()
+        print(
+            f"Total time to solve {len(puzzle_list)} puzzles was \
+            {str(time.time() - tot_start)} seconds"
+        )
 
-    puzzle_list = []
-    with open(filename, 'r', encoding="ascii") as file:
-        puzzle_list = file.readlines()
+    def run_single_puzzle(self, puzzle: str) -> None:
+        """Runs a single puzzle"""
 
-    for idx, puzzle in enumerate(puzzle_list):
         start = time.time()
-        puzzle = puzzle.strip()
-        solution = solve(puzzle, create_constraints(puzzle))
+        solution = self.solve(puzzle, self.create_constraints(puzzle))
+        print(
+            f"{'Puzzle':<10}: {puzzle}\n"
+            + f"{'Solution':<10}: {solution}\n"
+            + f"{'Time':<10}: {str(time.time() - start)} seconds\n"
+        )
 
-        print(f"{f'Puzzle {idx}':<12}: {puzzle}\n"
-            + f"{'Solution':<12}: {solution}\n"
-            + f"{'Time':<12}: {str(time.time() - start)} seconds\n")
+    def run(self, arg: str) -> None:
+        """Main runner"""
 
-    print(f'Total time to solve {len(puzzle_list)} puzzles was \
-          {str(time.time() - tot_start)} seconds')
-
-
-def run_single_puzzle(puzzle: str) -> None:
-    """ Runs a single puzzle
-    """
-
-    start = time.time()
-    solution = solve(puzzle, create_constraints(puzzle))
-    print(f"{'Puzzle':<10}: {puzzle}\n"
-        + f"{'Solution':<10}: {solution}\n"
-        + f"{'Time':<10}: {str(time.time() - start)} seconds\n")
-
-
-def run(arg: str):
-    """ Main runner
-    """
-
-    if arg[-4:] == '.txt':
-        run_file(arg)
-    else:
-        run_single_puzzle(arg)
+        if arg[-4:] == ".txt":
+            self.run_file(arg)
+        else:
+            self.run_single_puzzle(arg)
 
 
 def __main__():
+    args = sys.argv[1:]
+    solver = PuzzleSolver()
+
     if len(args) == 1:
-        run(args[0].strip())
+        solver.run(args[0].strip())
     elif len(args) == 0:
-        print("Incorrect arguments format\n"
-            + "See README for instructions.\n")
+        print("Incorrect arguments format.\n" + "See README for instructions.\n")
